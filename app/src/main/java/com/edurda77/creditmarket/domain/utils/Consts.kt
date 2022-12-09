72d03b98ae738dd5815978ebe9f2400332cab27d
hayarovy <edurd@rambler.ru>
2022-12-09 16:21:39 +0500

    release two color title

--- app/src/main/java/com/edurda77/creditmarket/domain/utils/Consts.kt
+++ app/src/main/java/com/edurda77/creditmarket/domain/utils/Consts.kt
@@ -2,3 +2,4 @@ package com.edurda77.creditmarket.domain.utils
 
 const val DESCRIPTION_ERROR = "Неизвестная ошибка"
 const val BASE_URL = "https://raw.githubusercontent.com/alexofyou/"
+const val TAKE_CREDIT = "ВЗЯТЬЗАЙМ"
--- app/src/main/java/com/edurda77/creditmarket/presentation/MainActivity.kt
+++ app/src/main/java/com/edurda77/creditmarket/presentation/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.edurda77.creditmarket.presentation
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.core.view.isVisible
 import com.edurda77.creditmarket.R
 import com.edurda77.creditmarket.databinding.ActivityMainBinding
+import com.edurda77.creditmarket.domain.utils.TAKE_CREDIT
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,6 +22,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        supportActionBar?.hide()
+        setTitle()
         viewModel.allData.observe(this) {
             when (it) {
                 is MainViewState.Success -> {
@@ -35,5 +42,12 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private fun setTitle() {
+        val spanText: Spannable = SpannableString (TAKE_CREDIT)
+        spanText.setSpan(ForegroundColorSpan(Color.parseColor("#4B4B4B")),0, 5, Spannable.SPAN_EXCLUSIVE_INCLUSIVE)
+        spanText.setSpan(ForegroundColorSpan(Color.parseColor("#4CDA64")),5, 9, Spannable.SPAN_EXCLUSIVE_INCLUSIVE)
+        binding.title.text = spanText
     }
 }
